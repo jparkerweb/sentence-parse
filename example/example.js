a59e5b34ad8cb7f85d6977cca7bbdaef4cea8fdb
Justin Parker <jparkerweb@gmail.com>
2025-01-13 14:11:01 -0800

    update example

--- example/example.js
+++ example/example.js
@@ -3,11 +3,14 @@ import { readFile } from 'fs/promises'
 import { join } from 'path'
 
 // Read the text file
-const text = await readFile(join(process.cwd(), '/example-text2.txt'), 'utf8')
+const text = await readFile(join(process.cwd(), '/example-text.txt'), 'utf8')
 
 const sentences = await parseSentences(text, { 
     observeMultipleLineBreaks: true,
-    removeStartLineSequences: ['~', '->'] 
+    removeStartLineSequences: ['>'],
+    preserveListItems: true,
+    listItemPrefix: '- ',
+    preserveHTMLBreaks: true,
 })
 
 for (const [index, sentence] of sentences.entries()) {
