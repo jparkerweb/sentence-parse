1eb71c2f4c82980a0ce2517256ec6bc85915d3e6
Justin Parker <jparkerweb@gmail.com>
2025-01-30 10:25:00 -0800

    update example

--- example/example.js
+++ example/example.js
@@ -1,9 +1,13 @@
 import { parseSentences } from "../sentenceParse.js"
 import { readFile } from 'fs/promises'
-import { join } from 'path'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// Get directory name regardless of how script is run
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // Read the text file
-const text = await readFile(join(process.cwd(), '/example-text.txt'), 'utf8')
+const text = await readFile(join(__dirname, 'example-text.txt'), 'utf8')
 
 const sentences = await parseSentences(text, { 
     observeMultipleLineBreaks: true,
